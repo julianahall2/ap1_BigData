31f6c85c9351b58fa3ae35f6ed7af0350b93117e
JULIANA AZEREDO HALL <202201037776@alunos.ibmec.edu.br>
2024-09-18 19:19:30 -0300

    projeto

---
+++ ap1/ap1_bigdata/src/main/java/ap1/bigdata/ap1_bigdata/Ap1BigdataApplication.java
@@ -0,0 +1,13 @@
+package ap1.bigdata.ap1_bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ap1BigdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ap1BigdataApplication.class, args);
+	}
+
+}
