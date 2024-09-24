e6ab215b8e1f8ccf593d31358b86aa2055a8b9c0
Juliana Hall <juju.azeredohall@gmail.com>
2024-09-24 16:09:49 -0300

    comecando

---
+++ src/main/java/ap1/bigdata/ap1_bigdata/model/Cliente.java
@@ -0,0 +1,11 @@
+package ap1.bigdata.ap1_bigdata.model;
+
+@Data
+public class Cliente {
+
+    private int id;
+    private String nome;
+    private Char cpf;
+    private String email; 
+
+}
---
+++ src/main/java/ap1/bigdata/ap1_bigdata/model/Endereco.java
@@ -0,0 +1,7 @@
+package ap1.bigdata.ap1_bigdata;
+
+
+@Data
+public class Endereco {
+
+}
